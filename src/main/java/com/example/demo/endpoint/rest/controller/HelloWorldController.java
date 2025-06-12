5dcd20ff01673a808970e26923dc00897bcf3a70
Minosoa <minoravelonirina@gmail.com>
2025-06-12 12:55:42 +0300

    feat: modify HelloWorldController file

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.exemple.demo.endpoint.rest.controller;
+package com.example.demo.endpoint.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
